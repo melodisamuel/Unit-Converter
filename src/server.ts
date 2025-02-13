1137b84d8788f2d920e50ed264f7ba9974221f80
Melody <melodysamuelcodes@gmail.com>
2025-02-13 10:09:45 +0100

    #feat update server.ts file

--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,31 @@ import  express  from "express";
 import path from 'path';
 import bodyParser from "body-parser";
 
-// import lengthRoutes from "../src/routes/lengthRoutes";
-// import weightRoutes from "../src/routes/weightRoutes";
-// import tempratureRoutes from "../src/routes/tempratureRoutes";
+import lengthRoutes from "../src/routes/lengthRoutes";
+import weightRoutes from "../src/routes/weightRoutes";
+import tempratureRoutes from "../src/routes/tempratureRoutes";
 
 
-const app = express()
+const app = express();
+const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-// Define Routes
-// app.use("/length", lengthRoutes);
-// app.use("/weight", weightRoutes);
-// app.use("/temprature", tempratureRoutes);
+Define Routes
+app.use("/length", lengthRoutes);
+app.use("/weight", weightRoutes);
+app.use("/temprature", tempratureRoutes);
 
 // Home Route
 app.get("/", (req, res) => {
-    res.send(`
-        <h1>Welcome to the Unit Converter</h1>
-        <a href="/length">Length</a> |
-        <a href="/weight">Weight</a> |
-        <a href="/temprature">Temprature</a>`
-    );
+    res.send("Welcome to the Unit Converter");
 });
 
 // Start Server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
