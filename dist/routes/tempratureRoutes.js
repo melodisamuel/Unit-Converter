daa9f40c52fd295f5a85655c41c3cc355b9d520e
Melody <melodysamuelcodes@gmail.com>
2025-02-14 08:13:29 +0100

    #feat update server.sts

--- dist/routes/tempratureRoutes.js
+++ dist/routes/tempratureRoutes.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const tempratureController_1 = require("../controllers/tempratureController");
+const router = express_1.default.Router();
+router.get("/", tempratureController_1.convertTemprature);
+router.post("/", tempratureController_1.convertTemprature);
+exports.default = router;
--- dist/routes/weightRoutes.js
+++ dist/routes/weightRoutes.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const weightController_1 = require("../controllers/weightController");
+const router = express_1.default.Router();
+router.get("/", weightController_1.convertWeight);
+router.post("/", weightController_1.convertWeight);
+exports.default = router;
